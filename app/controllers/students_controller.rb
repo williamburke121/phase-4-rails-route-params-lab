3c0d867f06f6e4223c0382f1b9ef4d151afaed7c
William Warner Burke <williamburke121@gmail.com>
2022-04-04 12:36:06 -0400

    commit tree ea65271dbb7a00b09566203c2fc0e78c28aeb3c2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    students = Student.find(params[:id])
+    render json: students
+  end
 
 end
